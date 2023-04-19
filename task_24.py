4bdb52a8141f7940965e8bb03ea34cae39773612
Igor Yakushev <igor.yakushev@gmail.com>
2023-04-19 13:31:18 +0300

    Correct task_24

--- task_24.py
+++ task_24.py
@@ -2,7 +2,7 @@ from random import randint
 gryadki = int(input("Введите количество грядок: "))
 yagody = [randint(1, 100) for i in range(gryadki)]
 print(yagody)
-maksimum = 0
+maksimum = yagody[0] + yagody[1] + yagody[len(yagody) - 1]
 for y in range(1, len(yagody) - 1):
     if maksimum < yagody[y-1] + yagody[y] + yagody[y+1]:
         maksimum = yagody[y-1] + yagody[y] + yagody[y+1]
